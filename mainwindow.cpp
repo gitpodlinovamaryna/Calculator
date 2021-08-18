cf9547d1397e27c4e026675638d2d00e10a0cacc
maryna <podlinovamaryna@gmail.com>
2021-08-18 03:17:14 +0300

    Fill GUI and add slot

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,27 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->pushButton_0,SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_1,SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_2,SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_3,SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_4,SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_5,SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_6,SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_7,SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_8,SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_9,SIGNAL(clicked()), this, SLOT(digits_numbers()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::digits_numbers()
+{
+    QPushButton *button = (QPushButton *)sender();
+    ui->result_show->setText(button->text());
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -17,5 +17,8 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+private slots:
+    void digits_numbers();
 };
 #endif // MAINWINDOW_H
